8e689316f786f991c205a916ad1959b0144f26e4
Jakub Stranianek <j.stranianek@gmail.com>
2023-08-01 14:50:17 +0200

    push project details

--- portfolio/app/projects/page.js
+++ portfolio/app/projects/page.js
@@ -4,60 +4,6 @@ import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 import data from "../data.json"
 
-const projects = [
-  {
-    id: 1,
-    date: "Jan 22, 2023",
-    name: "Villiz Barbershop",
-    description: "A modern, user-friendly website for a traditional barbershop, offering services, barber profiles and easy contact booking.",
-    slug: "villiz-barbershop",
-    href: "https://villizbarbershop.sk/",
-    customCss: "lg:row-span-2 lg:col-start-1"
-  },
-  {
-    id: 2,
-    date: "Jul 11, 2023",
-    name: "Nicol Studio",
-    description: "Minimalistic and elegant design showcasing an extensive portfolio of avant-garde hairstyles and tratment techniques.",
-    slug: "nicol-studio",
-    href: "https://nicolstudio.sk/",
-    customCss: "lg:row-span-1 lg:col-start-2"
-  },
-  {
-    id: 3,
-    date: "Jan 11, 2023",
-    name: "Clock App",
-    description: "A sleek and functional clock app that provides accurate timekeeping and multiple clock faces to suit various preferences. Simple, yet reliable.",
-    slug: "clock-app",
-    href: "https://clock-app-frntmntr.netlify.app/",
-    customCss: "lg:row-span-1 lg:col-start-2"
-  },
-  {
-    id: 4,
-    date: "Dec 20, 2022",
-    name: "Github finder App",
-    description: "Real-time search, filtering options, and detailed user profiles for seamless collaboration within the Github community.",
-    slug: "github-finder-app",
-    href: "https://github-finder-femntr.netlify.app/",
-  },
-  {
-    id: 5,
-    date: "Dec 20, 2022",
-    name: "Password generator App",
-    description: "A handy app to generate strong and secure passwords. Customizable options for length, character types, and easy one-click generation.",
-    slug: "password-generator-app",
-    href: "https://password-generator-femntr.netlify.app/",
-  },
-  {
-    id: 6,
-    date: "Oct 3, 2022",
-    name: "Design Landing Page",
-    description: "An eye-catching landing page with good looking visuals for showcasing design services or products. Engaging layout to capture visitors' attention and drive conversions.",
-    slug: "design-landing-page",
-    href: "https://courageous-florentine-a78dc8.netlify.app/",
-  },
-]
-
 
 export default function Projects() {
   return (
@@ -80,7 +26,7 @@ export default function Projects() {
         <div className="grid gap-8 grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 lg:gap-6">
           {data.filter(oneIndex => (oneIndex.id <= 3)).map(index => {
             return (
-              <div key={index.name} className={index.customCss}>              
+              <div key={index.name} className={index.id === 1 ? "lg:row-span-2 lg:col-start-1" : "lg:row-span-1 lg:col-start-2"}>              
               <Link href={"/projects/" + index.slug}>
               <Card>  
                   <div className='p-8 flex flex-col gap-4'>
@@ -100,7 +46,7 @@ export default function Projects() {
         <div className="grid gap-8 grid-cols-1 lg:grid-cols-3 lg:gap-6">
           {data.filter(oneIndex => (oneIndex.id > 3)).map(index => {
             return (
-              <div key={index.name} className={index.customCss}>              
+              <div key={index.name}>              
               <Link href={"/projects/" + index.slug}>
               <Card>  
                   <div className='p-8 flex flex-col gap-4'>
@@ -113,8 +59,7 @@ export default function Projects() {
               </div>
             )
           })}
-        </div>        
-        
+        </div>         
       </div>    
     </div>      
   </div>
