13ee74164982b32be914f8a83991ea5bf0e08acb
Jakub Stranianek <j.stranianek@gmail.com>
2023-08-09 15:02:41 +0200

    one more update

--- portfolio/app/contact/page.js
+++ portfolio/app/contact/page.js
@@ -41,7 +41,7 @@ export default function Contact() {
               {/* HEADER */}
               <section className={'py-8 w-full fixed z-50 backdrop-blur duration-200 bg-zinc-900/500 border-b border-zinc-600'}>
         <div className='relative w-4/5 h-full m-auto flex items-center justify-between'>
-        <Link href="/" alt="homePage"><ArrowLeftIcon className="relative z-20 w-8 text-zinc-400 hover:text-white" /></Link>
+        <Link href="/" alt="homePage"><ArrowLeftIcon className="relative z-20 w-8 text-zinc-400 hover:text-white" alt="homePage"/></Link>
           <Navbar />
         </div>
       </section>
@@ -52,12 +52,12 @@ export default function Contact() {
           <section className='grid gap-16 items-center grid-cols-1 lg:grid-cols-3'>
             {data.map(index => {
               return (
-                <Link href={index.href} key={index.name} alt={index.name + ": " + index.text} target={index.target}>
+                <Link href={index.href} key={index.name} target={index.target}>
                   <Card>
                     <div className='py-8 flex flex-col items-center lg:py-32'>
                       <div className='relative'>
                         <div className='border rounded-full border-zinc-400 p-3'>
-                          <Image src={index.icon} />               
+                          <Image src={index.icon} alt={index.name + ": " + index.text}/>               
                         </div>
                         <div className='absolute inset-0 translate-x-[23px] translate-y-[46px] z-10 w-[1px] h-[120px] bg-gradient-to-b from-zinc-400 to-transparent lg:h-[150px]'></div>
                       </div>
